25117fbf3fa6410855115e45dc97eb16fc3f3aef
satyam <codeML@localhost.localdomain>
2017-07-19 03:40:10 +0530

    Added stub for input

--- notes/views.py
+++ notes/views.py
@@ -1,5 +1,13 @@
-from notes import app
+from notes import app 
+from flask import render_template
+from flask import request
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html')
+
+@app.route('/submit',methods=['POST','GET'])
+def submission():
+
+    print (request.form['notes'])
+    return render_template('index.html')
